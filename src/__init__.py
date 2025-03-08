46520d09a9d38ef36392e53ef75d3b413b7a721d
antalbalint97 <antalbalint97@gmail.com>
2025-03-08 22:25:42 +0100

    Adding main.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,9 @@
 # Make key modules available at package level
 from .scraper import RecipeScraper
+from .scraper import IngredientScraper
 from .data_cleaner import DataProcessor
 # from .db_manager import DatabaseManager
 # from .analysis import analyze_data
 # from .model import RecipeModel
 
-__all__ = ["RecipeScraper", "DataProcessor"]
+__all__ = ["RecipeScraper", "IngredientScraper", "DataProcessor"]
